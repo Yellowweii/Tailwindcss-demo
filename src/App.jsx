a95082ab6e5561b72c606e887d78953400c4c17f
huang wei <138266079+yellowweii@users.noreply.github.com>
2023-09-22 22:20:27 +0800

    finish the function of navbar (#1)
    Co-authored-by: yellowwei <1119765689@qq.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import {Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer, Nav} from './components'
 
-
-export default function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t pb-8 bg-black">
+        <Footer />
+      </section>
+    </main>
   )
 }
+
+export default App
